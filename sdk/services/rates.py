e79f883ca033fed90c8de7f9d225863c0b5d9a4a
PaulWalcker <pashasimanko123@gmail.com>
2025-09-10 17:50:53 +0400

    feat(rates): add latest method

--- sdk/services/rates.py
+++ sdk/services/rates.py
@@ -5,4 +5,22 @@ class RatesService:
     def __init__(self, requester_service: RequesterService):
         self._request_service = requester_service
 
+    def latest(
+            self,
+            base: str | None = None,
+            symbols: list[str] | None = None,
+            pretty_print=False,
+            show_alternative=False
+    ):
+        params = {
+            'pretty_print': pretty_print,
+            'show_alternative': show_alternative
+        }
 
+        if base is not None:
+            params['base'] = base
+
+        if symbols is not None:
+            params['symbols'] = symbols
+
+        return self._request_service.get('/latest.json', params)
