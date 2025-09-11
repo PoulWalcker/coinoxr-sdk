7f04a3f28c44efb059c65feeacb9cce86c808759
PaulWalcker <pashasimanko123@gmail.com>
2025-09-11 14:53:56 +0400

    chore(client): expand client`s modules

--- sdk/client.py
+++ sdk/client.py
@@ -1,5 +1,9 @@
 from sdk.services.currencies import CurrenciesService
+from sdk.services.rates import RatesService
 from sdk.services.requester import RequesterService
+from sdk.services.converter import ConverterService
+from sdk.services.ohlc import OhlcService
+from sdk.services.usage import UsageService
 
 from sdk.httpp.request_client import RequestsClient
 
@@ -10,3 +14,7 @@ class CoinoxrClient:
         self._requester_service = RequesterService(app_id, self.client, base_url)
 
         self.currencies = CurrenciesService(self._requester_service)
+        self.rates = RatesService(self._requester_service)
+        self.converter = ConverterService(self._requester_service)
+        self.ohlc = OhlcService(self._requester_service)
+        self.usage = UsageService(self._requester_service)
