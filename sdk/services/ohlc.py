8cddc7529bc9e5f56c191271a45b0673e3dea5dd
PaulWalcker <pashasimanko123@gmail.com>
2025-09-11 14:41:48 +0400

    fix(ohlc): add ohlc method

---
+++ sdk/services/ohlc.py
@@ -0,0 +1,34 @@
+from sdk.services.requester import RequesterService
+from sdk.utils.dates import ensure_date
+
+
+class OhlcService:
+    def __init__(self, requester_service: RequesterService):
+        self._request_service = requester_service
+
+    def ohlc(
+            self,
+            start: str,
+            period: str,
+            base: str | None = None,
+            symbols: str | None = None,
+            pretty_print=False,
+            show_alternative=False
+    ):
+        if not ensure_date(start, "%Y-%m-%dT%H:%M:%SZ"):
+            raise ValueError("Invalid datetime format, expected YYYY-MM-DDThh:mm:ssZ")
+
+        params = {
+            'start': start,
+            'period': period,
+            'pretty_print': pretty_print,
+            'show_alternative': show_alternative
+        }
+
+        if base is not None:
+            params['base'] = base
+
+        if symbols is not None:
+            params['symbols'] = symbols
+
+        return self._request_service.get('/ohlc.json', params)
