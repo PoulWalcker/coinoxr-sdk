f1e02c840e7a55535ea2671960ee5136fe21c1ce
PaulWalcker <pashasimanko123@gmail.com>
2025-09-15 17:30:37 +0400

    chore(utils): lint code

--- tests/test_utils_date.py
+++ tests/test_utils_date.py
@@ -12,21 +12,27 @@ def test_ensure_date_time_valid():
     assert ensure_date(valid_iso, '%Y-%m-%dT%H:%M:%SZ')
 
 
-@pytest.mark.parametrize('bad_date', [
-    "2025/12/01",   # wrong separator
-    "2025-13-01",   # invalid month
-    "2025-07-35",   # invalid day
-    "string",       # not a date at all
-])
+@pytest.mark.parametrize(
+    'bad_date',
+    [
+        "2025/12/01",  # wrong separator
+        "2025-13-01",  # invalid month
+        "2025-07-35",  # invalid day
+        "string",  # not a date at all
+    ],
+)
 def test_ensure_date_fail(bad_date):
     assert ensure_date(bad_date, '%Y-%m-%d') is False
 
 
-@pytest.mark.parametrize("bad_date", [
-    "2025/12/01",                  # wrong format
-    "2025-12-01T25-12-30Z",        # invalid hour
-    "2025-12-01T12-70-30Z",        # invalid minute
-    "2025-12-01T12-12-98Z",        # invalid second
-])
+@pytest.mark.parametrize(
+    "bad_date",
+    [
+        "2025/12/01",  # wrong format
+        "2025-12-01T25-12-30Z",  # invalid hour
+        "2025-12-01T12-70-30Z",  # invalid minute
+        "2025-12-01T12-12-98Z",  # invalid second
+    ],
+)
 def test_ensure_datetime_invalid(bad_date):
     assert ensure_date(bad_date, "%Y-%m-%dT%H:%M:%SZ") is False
