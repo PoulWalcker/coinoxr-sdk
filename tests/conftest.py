25e48de1606f5ff2f65d49a92106752592d03613
PaulWalcker <pashasimanko123@gmail.com>
2025-09-16 16:39:32 +0400

    test(conftest): update fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@ import pytest
 from sdk.transport.base import HttpClient
 from sdk.services.requester import RequesterService
 from sdk.transport.stub_client import StubClient
+from sdk.services.currencies import CurrenciesService
+from sdk.services.rates import RatesService
+from sdk.services.converter import ConverterService
+from sdk.services.ohlc import OhlcService
 
 
 @pytest.fixture
@@ -12,3 +16,23 @@ def stub_client() -> HttpClient:
 @pytest.fixture
 def requester(stub_client: HttpClient) -> RequesterService:
     return RequesterService(app_id='test_key', client=stub_client)
+
+
+@pytest.fixture
+def currencies_services(requester):
+    return CurrenciesService(requester)
+
+
+@pytest.fixture
+def rates_services(requester):
+    return RatesService(requester)
+
+
+@pytest.fixture
+def converter_service(requester):
+    return ConverterService(requester)
+
+
+@pytest.fixture
+def ohlc_service(requester):
+    return OhlcService(requester)
